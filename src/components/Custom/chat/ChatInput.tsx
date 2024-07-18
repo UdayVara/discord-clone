314bb9afae2c14b1871e4432a521431508748573
Uday vara <varaudayd@gmail.com>
2024-07-19 00:36:29 +0530

    --->Added Emoji Picker Layout

--- src/components/Custom/chat/ChatInput.tsx
+++ src/components/Custom/chat/ChatInput.tsx
@@ -4,16 +4,21 @@ import { Textarea } from "@/components/ui/textarea";
 import React from "react";
 import { FaFileAlt } from "react-icons/fa";
 import { IoSend } from "react-icons/io5";
+import { GrEmoji } from "react-icons/gr";
 function ChatInput() {
   return (
     <>
       <div className="flex flex-row      bottom-0 dark:bg-neutral-900/70">
-        <FaFileAlt className="text-5xl p-4    " />
-        <Textarea 
+        <GrEmoji className="text-5xl p-3    " />
+        <Textarea
+          rows={1}
           placeholder="Enter Message Here"
-          className="grow focus-visible:ring-0 border-0 ouline-none    outline-none dark:bg-neutral-900/70 h-2 focus-visible:ring-offset-0"
+          className="grow resize-none focus-visible:ring-0 border-0 ouline-none    outline-none dark:bg-neutral-900/70 focus-visible:ring-offset-0"
         />
-        <IoSend className="text-5xl p-3 text-indigo-600 " />
+        <div className="flex gap-1 items-center">
+          <FaFileAlt className="text-5xl p-4    " />
+          <IoSend className="text-5xl p-3 text-indigo-600 " />
+        </div>
       </div>
     </>
   );
