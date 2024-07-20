416d3d11cf06cc358e13aa38b3e3a6afe5c607a4
Uday vara <varaudayd@gmail.com>
2024-07-20 23:39:43 +0530

    --->Added Emoji field and fixed Socket Connection Failure

--- src/Providers/context/SocketContextProvider.tsx
+++ src/Providers/context/SocketContextProvider.tsx
@@ -1,19 +1,19 @@
-"use client"
+"use client";
 
+import React from "react";
+import { SocketContext } from "./socketContext";
+import { io, Socket } from "socket.io-client";
 
-import React from 'react'
-import {SocketContext } from './socketContext'
-import {io, Socket} from "socket.io-client"
-
-function SocketContextProvider({children}:{children:React.ReactNode}) {
-     const socketConnection = io(process.env.NEXT_PUBLIC_SOCKET_URL as string,{
-      transports:["websocket","polling"]
-     })
+function SocketContextProvider({ children }: { children: React.ReactNode }) {
+  const socketConnection = io(process.env.NEXT_PUBLIC_SOCKET_URL as string, {
+    transports: ["websocket","polling"],
+  });
+  console.debug("Socket", socketConnection);
   return (
     <SocketContext.Provider value={socketConnection}>
-        {children}
+      {children}
     </SocketContext.Provider>
-  )
+  );
 }
 
-export default SocketContextProvider
+export default SocketContextProvider;
--- src/components/Custom/chat/ChatInput.tsx
+++ src/components/Custom/chat/ChatInput.tsx
@@ -1,24 +1,47 @@
 "use client";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import React from "react";
+import React, { useState } from "react";
 import { FaFileAlt } from "react-icons/fa";
 import { IoSend } from "react-icons/io5";
 import { GrEmoji } from "react-icons/gr";
+import EmojiPicker, { Theme } from "emoji-picker-react";
+import { useTheme } from "next-themes";
+
 function ChatInput() {
+  const [open, setOpen] = useState(false);
+
+  const theme: any = useTheme();
   return (
     <>
-      <div className="flex flex-row      bottom-0 dark:bg-neutral-900/70">
-        <GrEmoji className="text-5xl p-3    " />
-        <Textarea
-          rows={1}
-          placeholder="Enter Message Here"
-          className="grow resize-none focus-visible:ring-0 border-0 ouline-none    outline-none dark:bg-neutral-900/70 focus-visible:ring-offset-0"
-        />
-        <div className="flex gap-1 items-center">
-          <FaFileAlt className="text-5xl p-4    " />
-          <IoSend className="text-5xl p-3 text-indigo-600 " />
+      <div className="flex-col flex w-full">
+        <div className="flex flex-row items-center     bottom-0 dark:bg-neutral-900/70">
+          <GrEmoji
+            className="text-5xl p-3    "
+            onClick={() => {
+              setOpen(!open);
+            }}
+          />
+          <Textarea
+            rows={1}
+            placeholder="Enter Message Here"
+            className="grow resize-none focus-visible:ring-0 border-0 ouline-none min-h-full text-base pt-3  outline-none dark:bg-neutral-900/70 focus-visible:ring-offset-0"
+          />
+          <div className="flex gap-1 items-center">
+            <div className="relative hover:bg-neutral-800 duration-200 cursor-pointer transition-all">
+              <input type="file" name="" className="w-full h-full absolute opacity-0" id="" />
+            <FaFileAlt className="text-5xl p-4    " />
+            </div>
+            <IoSend className="text-5xl p-3 text-indigo-600 " />
+          </div>
         </div>
+        <EmojiPicker
+         
+          style={{ width: "100% !important",maxHeight:"40vh !important" }}
+          open={open}
+          className={`${open ? "scale-100" : "scale-0"}`}
+          theme={theme.resolvedTheme || "dark"}
+        />
       </div>
     </>
   );
