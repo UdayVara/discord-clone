828cf017ad5b0fb831eca0fda387e0662f9ba381
Uday vara <varaudayd@gmail.com>
2024-08-14 00:39:47 +0530

    -->Fixed Padding

--- src/app/(main)/server/[serverId]/page.tsx
+++ src/app/(main)/server/[serverId]/page.tsx
@@ -16,7 +16,7 @@ export default function Page({ params }: { params: { serverId: string } }) {
   const [start, setStart] = useState(false);
   return (
     <>
-      <div className="w-full h-screen flex flex-col md:pb-0 pb-20">
+      <div className="w-full h-screen flex flex-col md:pb-0 pb-16">
         {selectedChannel.channelId && selectedChannel.channelId != "" ? (
           <>
             <ChatHeader />
