5488664ddea322af2634d7cee36eeda9d152c51b
Uday vara <varaudayd@gmail.com>
2024-08-15 01:08:08 +0530

    --->Updated Gitignore --->Fixed Socket Connection Error --->Updated Socket Error issue

--- src/Providers/context/SocketContextProvider.tsx
+++ src/Providers/context/SocketContextProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { SocketContext } from "./socketContext";
 import { io, Socket } from "socket.io-client";
 import { toast } from "sonner";
@@ -8,25 +8,29 @@ import { useAuth } from "@/hooks/useAuth";
 
 function SocketContextProvider({ children }: { children: React.ReactNode }) {
   const user = useAuth();
-  let socketConnection: Socket | null = null;
+  let socketConnection = useRef<Socket | null>(null);
 
-  if (user && user.id) {
-    socketConnection = io(process.env.NEXT_PUBLIC_SOCKET_URL as string, {
-      transports: ["websocket", "polling"],
-      auth: {
-        token: "Bearer " + user.token,
-        id:user.id
-      },
-    });
-
-    socketConnection.on("error", (data) => {
-      console.debug("Error Received", data);
-      toast.error(data.message || "Internal Server Error");
-    });
-  }
+  useEffect(()=>{
+    console.debug("user",user)
+    if (user && user.id) {
+      socketConnection.current = io(process.env.NEXT_PUBLIC_SOCKET_URL as string, {
+        transports: ["websocket", "polling"],
+        auth: {
+          token: "Bearer " + user.token,
+          id:user.id
+        },
+      });
+  
+      socketConnection.current.on("error", (data) => {
+        console.debug("Error Received", data);
+        toast.error(data.message || "Internal Server Error");
+      });
+    }
+  },[user])
+  
 
   return (
-    <SocketContext.Provider value={socketConnection as Socket}>
+    <SocketContext.Provider value={socketConnection.current as Socket}>
       {children}
     </SocketContext.Provider>
   );
--- src/Providers/context/socketContext.ts
+++ src/Providers/context/socketContext.ts
@@ -1,7 +1,7 @@
 "use client"
 
 import { createContext } from "react";
-import { connect } from "socket.io-client";
+
 
 
 export const SocketContext = createContext({});
