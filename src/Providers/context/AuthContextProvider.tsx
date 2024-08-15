d04ae4a369bc1b33298118f09ef055ea88ad531a
Uday vara <varaudayd@gmail.com>
2024-08-15 23:34:42 +0530

    --->Bug Fixes

---
+++ src/Providers/context/AuthContextProvider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { AuthContext } from "./authContext";
+import { getUser } from "@/actions/Auth.action";
+
+ const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<any>(null); // Start with `null` instead of an empty object.
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await getUser();
+      console.debug("User Res",res.user)
+      setUser(res?.user || null); // Use optional chaining and start with `null`.
+    };
+
+    fetchUser();
+  }, []);
+
+  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+};
+
+
+export default AuthContextProvider;
